e9630e3ed5ee15f0614eeb5f3131288461ff46bb
Rionaldi Lausiri <rnaldi491@gmail.com>
2022-05-16 18:31:28 +0900

    menambahkan beberapa file

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+
+
+// ini contoh print sesuatu ke layar pada javascript
+document.writeln("<h1> hallo Bugs </h1>");
+document.writeln("<p> hallo Bugs </p>"); // ini juga komentar 
+
+// ini komentar satu baris
+
+/* 
+ini
+komentar
+multiline/multi baris
+*/ 
+
